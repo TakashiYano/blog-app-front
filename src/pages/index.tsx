77513457e909a7573403322e19f5b55bbb7aca70
TakashiYano <0tp6324w3261z0v@gmail.com>
2021-03-23 07:10:51 +0000

    🩹 微修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,65 +1,51 @@
-import Head from "next/head";
+import type { NextPage } from "next";
+import Link from "next/link";
+import { HeaderNavigation } from "src/components/separate/HeaderNavigation";
 import { Layout } from "src/components/separate/Layout";
-import { HeaderNavigation } from "src/components/shared/HeaderNavigation";
-import { MainDivider } from "src/components/shared/MainDivider";
-import { MainGuide } from "src/components/shared/MainGuide";
-import { PrimaryButton } from "src/components/shared/PrimaryButton";
-import type { User, UserPutRequest } from "src/models/user";
-import useSWR from "swr";
 
-const Home = () => {
-  const { data, error } = useSWR<User>("/users/var");
+const PAGES = [
+  { href: "/login", label: "ログインページ", isDone: true },
+  { href: "/signup", label: "新規登録ページ", isDone: false },
+  { href: "/registration", label: "初回プロフィール登録ページ", isDone: false },
+  { href: "/user/foo", label: "ユーザーページ", isDone: false },
+  { href: "/search", label: "投稿検索ページ", isDone: true },
+  { href: "/post/foo", label: "投稿ページ", isDone: false },
+  { href: "/settings", label: "設定一覧ページ", isDone: false },
+  { href: "/settings/profile", label: "プロフィール変更ページ", isDone: false },
+  { href: "/settings/account", label: "SNS連携ページ", isDone: false },
+  { href: "/settings/notification", label: "通知設定ページ", isDone: false },
+  { href: "/terms", label: "利用規約ページ", isDone: true },
+  { href: "/terms/transaction-law", label: "特定商取引法ページ", isDone: true },
+  { href: "/privacy", label: "プライバシーポリシーページ", isDone: true },
+] as const;
 
-  const handleClick = async () => {
-    const req: UserPutRequest = { id: "var", name: "user1" };
-    // eslint-disable-next-line no-console
-    console.log({ ブラウザから送るリクエスト: req });
-    const res = await fetch("/users/var", {
-      method: "put",
-      body: JSON.stringify(req),
-    });
-    const json = await res.json();
-    // eslint-disable-next-line no-console
-    console.log({ サーバーから受け取ったレスポンス: json });
-  };
-
+const Index: NextPage = () => {
   return (
     <Layout>
-      <Head>
-        <title>Home</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <HeaderNavigation />
       <div className="px-5 py-14">
         <div
           className="px-5 py-8 mx-auto my-0 bg-white border border-gray-300 dark:border-gray-500 rounded-md dark:bg-gray-800 md:px-10"
           style={{ maxWidth: "960px", minWidth: "420px" }}
         >
-          <h2 className="dark:text-white">Home</h2>
-          <PrimaryButton className="px-5 py-2 font-bold" variant="outline" button onClick={handleClick}>
-            Button
-          </PrimaryButton>
-        </div>
-      </div>
-      <MainDivider />
-      <div className="px-5 py-14">
-        <div className="mx-auto my-0" style={{ maxWidth: "960px", minWidth: "420px" }}>
-          <MainGuide title="Featured" description="おすすめユーザーを見つけよう！" />
-          <div className="mt-4 dark:text-white">
-            {error ? <div>failed to load</div> : null}
-            {data ? (
-              <div>
-                <img src={data.profileImage} alt={data.name} width={80} height={80} />
-                <h2>{data.name}</h2>
-              </div>
-            ) : (
-              <div>loading...</div>
-            )}
-          </div>
+          <h1 className="text-2xl font-bold dark:text-white">ページ一覧</h1>
+          <ul className="mt-2 grid gap-4 grid-flow-row sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {PAGES.map((page) => {
+              return (
+                <li key={page.href}>
+                  <Link href={page.href}>
+                    <a className="block p-3 border border-gray-300 dark:text-white dark:border-gray-500">{`${
+                      page.isDone ? "✅ " : ""
+                    }${page.label}`}</a>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </div>
     </Layout>
   );
 };
 
-export default Home;
+export default Index;
